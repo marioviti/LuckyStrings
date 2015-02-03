b95a908c31a94d90204e1737bc35cd4529577274
Ben Stokes <benhstokes@gmail.com>
2015-02-03 14:45:37 +0000

    Here you go

--- searchApp/connection.js
+++ searchApp/connection.js
@@ -4,9 +4,9 @@ var connection = mysql.createConnection(
     {
       host     : 'localhost',
       user     : 'root',
-      password : 'Ianbru32',
+      password : 'root',
       database : 'museum',
-      port : 3306
+      port : 8889
     }
 );
  
