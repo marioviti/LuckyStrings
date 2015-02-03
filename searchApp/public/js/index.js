be9f305198da8140b8dd20bcb867b32f29d2f534
Ben Stokes <benhstokes@gmail.com>
2015-02-03 10:20:36 +0000

    added mapplic

--- searchApp/public/js/index.js
+++ searchApp/public/js/index.js
@@ -14,6 +14,20 @@ roomLocations["Chinese Glass Gallery"] = new Array(688,120);
 roomLocations["Sea Dragons Gallery"] = new Array(153,225);
 roomLocations["South West Wildlife Gallery"] = new Array(50,380);
 
+var stuffInRooms = new Array();
+  stuffInRooms["Glass Gallery"] = "";
+  stuffInRooms["Geology Gallery"] = "";
+  stuffInRooms["Eastern Art Gallery"] = "";
+  stuffInRooms["World Wildlife Gallery"] = "";
+  stuffInRooms["Wills (Rear) Hall"] = "";
+  stuffInRooms["British Wildlife Gallery"]= "";
+  stuffInRooms["Egypt Gallery"]= new Array(250,370);
+  stuffInRooms["Ground Floor Lift Foyer"] = "";
+  stuffInRooms["Curiosity"] = "";
+  stuffInRooms["Winterstoke (Front) Hall"] = "";
+  stuffInRooms["Chinese Glass Gallery"] = "";
+  stuffInRooms["Sea Dragons Gallery"] = "";
+  stuffInRooms["South West Wildlife Gallery"] = "";
 
 $('form').submit(function(){
   socket.emit('chat message', $('#m').val());
@@ -25,7 +39,8 @@ socket.on('chat message', function(msg){
 
   $('#messages').empty();
   $('#floorplanTags').empty();
-
+  var taggedLocations = [];
+  var x=0;
   var rows = msg.split('\n');
   for (var i in rows){
     var item = rows[i].split('- ');
@@ -43,7 +58,29 @@ socket.on('chat message', function(msg){
       +'</a>';
     $("#messages").append(listAppended);
     if(itemCoords!=null){
-      $("#floorplanTags").append('<img src="img/tag.png" class="tagImg animated bounceInDown" style="top: '+itemCoords[1]+'px; left: '+itemCoords[0]+'px;">');
+      if($.inArray(location, taggedLocations)==-1){
+        $("#floorplanTags").append('<img src="img/tag.png" id="'+location+'" tabindex="50" class="tagImg animated bounceInDown" data-placement="top" style="top: '+itemCoords[1]+'px; left: '+itemCoords[0]+'px;">');
+        taggedLocations[x] = location;
+        x++;
+      }
+      stuffInRooms[location] = stuffInRooms[location]+" "+itemInformation;
     }
   }
 });
+
+
+function resetRooms(){
+stuffInRooms["Glass Gallery"] = "";
+stuffInRooms["Geology Gallery"] = "";
+stuffInRooms["Eastern Art Gallery"] = "";
+stuffInRooms["World Wildlife Gallery"] = "";
+stuffInRooms["Wills (Rear) Hall"] = "";
+stuffInRooms["British Wildlife Gallery"]= "";
+stuffInRooms["Egypt Gallery"]= "";
+stuffInRooms["Ground Floor Lift Foyer"] = "";
+stuffInRooms["Curiosity"] = "";
+stuffInRooms["Winterstoke (Front) Hall"] = "";
+stuffInRooms["Chinese Glass Gallery"] = "";
+stuffInRooms["Sea Dragons Gallery"] = "";
+stuffInRooms["South West Wildlife Gallery"] = "";
+}
