ee20e29cd722db84d929c9eac10c644a72676a40
mario viti <marioviti@eduroam-211-108.nomadic.bris.ac.uk>
2015-02-04 12:30:19 +0000

    removed socket and added json scaffold

--- searchApp/routes/index.js
+++ searchApp/routes/index.js
@@ -1,11 +1,33 @@
 var express = require('express');
+var connection = require('../connection.js');
 var router = express.Router();
+var queryDb = require('../queryDB.js');
+
+connection.connect();
 
 
 /* GET home page. */
 router.get('/', function(req, res) {
 
-	console.log(req.query.search)
+  	res.render('index', { title: 'searchMuseum' });
+
+});
+
+/* GET home page. */
+router.get('/form', function(req, res) {
+
+	var urlObj=url.parse(req.url,true);
+
+	var json = urlObj.query;
+	
+	queryDb(urlObj.query.search,connection,function(data,err){
+
+			console.log(data);
+			res.json(data);
+
+		});
+  	}
+
   	res.render('index', { title: 'searchMuseum' });
 
 });
